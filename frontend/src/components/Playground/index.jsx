223c57607b032f161500ce39a07f41694cc129c1
thunderfern <xyfernx@gmail.com>
2024-03-30 17:29:46 -0400

    stuff

--- frontend/src/components/Playground/index.jsx
+++ frontend/src/components/Playground/index.jsx
@@ -1,4 +1,12 @@
 const Playground = () => {
-  return <div> here you go </div>
+  return (
+    <div>
+        <form action="/add-user" method="POST">
+            <input type="text" name="name"></input>
+            <input type="submit"></input>
+        </form>
+        <p>hi</p>
+    </div>
+  )
 }
 export default Playground
